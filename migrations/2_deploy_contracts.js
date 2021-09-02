dbe61e2ce7060247f4cb982613d4800ccc105dc1
FalconHex <joyal.palackel19@gmail.com>
2021-09-02 17:23:19 +0530

    BlockStore

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,6 @@
-//Import Contractconst DStorage = artifacts.require("DStorage");
+const DStorage = artifacts.require("DStorage");
 
 module.exports = function(deployer) {
 	//Deploy Contract
+	deployer.deploy(DStorage);
 };
--- src/contracts/DStorage.sol
+++ src/contracts/DStorage.sol
@@ -1,37 +1,77 @@
 pragma solidity ^0.5.0;
 
 contract DStorage {
-  // Name
-  // Number of files
-  // Mapping fileId=>Struct 
+    // Name
+    string public name = 'DStorage';
 
-  // Struct
+    // Number of files
+    uint public fileCount = 0;
 
+    // Mapping fileId=>Struct
+    // will store like
+    // KEY: 1 => VALUE: EIHUIjkdjihjfnjdhodo837784bbjfdhfe8
+    // KEY: 2 => VALUE: EPOKDPKPDjndihfnfj9393nljdnjsvnskjn
+    mapping( uint => File ) public files;
 
-  // Event
-
-  constructor() public {
-  }
-
-  // Upload File function
-
-    // Make sure the file hash exists
-
-    // Make sure file type exists
-
-    // Make sure file description exists
-
-    // Make sure file fileName exists
-
-    // Make sure uploader address exists
-
-    // Make sure file size is more than 0
+    // Struct: our own datatype
+    struct File {
+        uint fileId;
+        string fileHash;
+        uint fileSize;
+        string fileType;
+        string fileName;
+        string fileDescription;
+        uint uploadTime; // we use now to get current time
+        address payable uploader; // we use msg.sender to get address
+    }
 
 
-    // Increment file id
+    // Event
+    event FileUploaded(
+        uint fileId,
+        string fileHash,
+        uint fileSize,
+        string fileType,
+        string fileName,
+        string fileDescription,
+        uint uploadTime,
+        address payable uploader
+    );
 
-    // Add File to the contract
+    constructor() public {}
 
-    // Trigger an event
+    // Upload File function
+    function uploadFile(string memory _fileHash, uint _fileSize, string memory _fileType, string memory _fileName, string memory _fileDescription) public {
+
+        // Make sure the file hash exists
+        require(bytes(_fileHash).length > 0);
+
+        // Make sure file type exists
+        require(bytes(_fileType).length > 0);
+
+        // Make sure file description exists
+        require(bytes(_fileDescription).length > 0);
+
+        // Make sure file fileName exists
+        require(bytes(_fileName).length > 0);
+
+        // Make sure uploader address exists
+        require(msg.sender != address(0));
+
+        // Make sure file size is more than 0
+        require(_fileSize > 0);
+
+        
+        // Increment file id
+        fileCount ++;
+        // Add File to the contract
+        files[fileCount] = File(fileCount, _fileHash, _fileSize, _fileType, _fileName, _fileDescription, now, msg.sender);
+
+        
+
+        // Trigger an event
+        emit FileUploaded(fileCount, _fileHash, _fileSize, _fileType, _fileName, _fileDescription, now, msg.sender);
+    }
+
 
 }
